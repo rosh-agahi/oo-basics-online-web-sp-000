1424485ecf28bf603b3c48b5fb45ee4232012555
Roshanak Agahi <rosh.agahi@gmail.com>
2019-09-17 18:18:38 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,7 +1,15 @@
 class Book 
-attr_accessor :title
+attr_accessor :title :author :page_count :genre
 
-def initialize(title)
+def initialize(title, auther, num, genre)
   @title = title
+  @author = author
+  @page_count = num
+  @genre = genre
 end
+
+def turn_page
+  puts "Flipping the page...wow, you read fast!"
+end
+
 end
